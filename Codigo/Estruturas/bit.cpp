c421efeeb58a0becb67dd0bddbb3c3a6e5c32631
G15-campfor <gabriel.campfor@gmail.com>
2025-11-13 23:58:58 -0300

    refactor: update BIT template

--- Codigo/Estruturas/bit.cpp
+++ Codigo/Estruturas/bit.cpp
@@ -5,26 +5,35 @@
 // query - O(log(n))
 
 struct BIT {
-	vector<ll> elements;
-	int size;
+	vector<ll> bit;
+	int n;
 
-	BIT(int _size) {
-		elements = vector<ll>(_size);
-		size = _size;
+	BIT(int n) {
+		this->n = n;
+		bit.assign(n, 0);
 	}
 
-	void update(int index, ll delta) {
-		for(int i = index; i < size; i += i&-i) {
-			elements[i] += delta;
+	BIT(vector<ll> const &a) : BIT(a.size()) {
+		for (int i = 0; i < a.size(); ++i) {
+			bit[i] += a[i];
+			int r = i | (i + 1);
+			if (r < n) bit[r] += bit[i];
 		}
 	}
 
-	ll query(int index) {
-		ll sum = 0;
-		for(int i = index; i > 0; i -= i&-i) {
-			sum += elements[i];
-		}
+	ll sum(int r) {
+		ll ret = 0;
+		for (; r >= 0; r = (r & (r + 1)) - 1)
+			ret += bit[r];
+		return ret;
+	}
 
-		return sum;
+	ll sum(int l, int r) {
+		return sum(r) - sum(l - 1);
+	}
+
+	void add(int idx, ll delta) {
+		for (; idx < n; idx = idx | (idx + 1))
+			bit[idx] += delta;
 	}
 };
